d933ca1e6fa451144a45082d24a6d4c3cf2a6205
Kleiton Ewerton <kleiton.ewerton@estudante.ufjf.br>
2023-09-04 15:52:29 -0300

    add help and nother commands

--- src/main/java/EchoClient.java
+++ src/main/java/EchoClient.java
@@ -1,9 +1,12 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class EchoClient {
+
     public static void main(String[] args) throws Exception {
+        Msg msgs = new Msg();
         String screenName = args[0];
         String host = args[1];
         int port = 4444;
@@ -13,17 +16,22 @@ public class EchoClient {
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         In in = new In(socket);
         Out out = new Out(socket);
-        System.err.println("Conectado a " + host + " na porta " + port);
+        System.err.println("Conectado a " + host + " na porta " + port + " - Use '@help' for help!");
+
+        // Variável final para indicar se a conexão ainda está ativa
+        final boolean[] isConnectionActive = {true};
 
         // Thread para ler mensagens do servidor e imprimir na saída padrão
         Thread serverReaderThread = new Thread(() -> {
             try {
                 String serverResponse;
-                while ((serverResponse = in.readLine()) != null) {
+                while (isConnectionActive[0] && (serverResponse = in.readLine()) != null) {
                     System.out.println(serverResponse);
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                if (isConnectionActive[0]) {
+                    e.printStackTrace();
+                }
             }
         });
         serverReaderThread.start();
@@ -31,14 +39,25 @@ public class EchoClient {
         // Lê da entrada padrão, envia e escreve resposta
         String line;
         while ((line = stdin.readLine()) != null) {
+
             // Leitura da entrada padrão
-            out.println("[" + screenName + "]: " + line);
+            if (line.equals("@quit")) {
+                isConnectionActive[0] = false; // Define a conexão como inativa
+                break;
+            }
+
+            switch (line) {
+                case "@help" -> msgs.helpMsg();
+                default -> out.println("[" + screenName + "]: " + line);
+            }
+
         }
 
         // Encerra os sockets
         System.err.println("Conexão com " + host + " encerrada");
+        socket.close();
         out.close();
         in.close();
-        socket.close();
     }
+
 }
---
+++ src/main/java/Msg.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kleit
+ */
+public class Msg {
+
+    public void helpMsg() {
+        System.out.println(" - '@help' : for help \n - '@quit' : for quit \n - '@echo' : for broadcast mensage");
+    }
+}
